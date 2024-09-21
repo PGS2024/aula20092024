e7d4ecfa773c90673e15e7190422b27f8a674dbe
Paulo GS2024 <164101438+PGS2024@users.noreply.github.com>
2024-09-20 21:26:42 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,17 @@
+let numeros = [1, 2, 3, 4, 5];
+
+let frutas = ["maça" , "banana", "laranja"];
+
+
+let nomes = [];
+nomes[0] = "joão"
+nomes[1] = "Maria"
+
+let cores = new Array("vermelho", "Azul", "Verde");
+let arrayVazio = new Array(5);
+
+console.log(numeros);
+
+console.log(frutas);
+
+console.log(nomes);
